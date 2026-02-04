da15b11134f8ac6ef77f1cdd0af7a365c732476a
suniljakkula <sunij8911@gmail.com>
2026-02-04 08:39:58 +0530

    first commit of the arrays

---
+++ src/com/java/arrays/basics/LengthOftheArray.java
@@ -0,0 +1,11 @@
+package com.java.arrays.basics;
+
+public class LengthOftheArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 3, 4 };
+		System.out.println("Length of the Array: " + arr.length);
+	}
+
+}
