dfb325e4c575849473dfd35a92c6d90c451e47ff
suniljakkula <sunij8911@gmail.com>
2026-02-08 08:35:43 +0530

    modified the code

--- src/com/arrays/basics/MoveAllZerosEndOfAnArray.java
+++ src/com/arrays/basics/MoveAllZerosEndOfAnArray.java
@@ -7,6 +7,7 @@ public class MoveAllZerosEndOfAnArray {
 		int n = arr.length;
 		int[] temp = new int[n];
 		int j = 0;
+		
 		for (int i = 0; i < n; i++) {
 			if (arr[i] != 0)
 				temp[j++] = arr[i];
