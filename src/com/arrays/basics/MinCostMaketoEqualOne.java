b4debc06d5d9e49e467c47725f83700550c06678
suniljakkula <sunij8911@gmail.com>
2026-02-09 05:56:03 +0530

    min cost

---
+++ src/com/arrays/basics/MinCostMaketoEqualOne.java
@@ -0,0 +1,23 @@
+package com.arrays.basics;
+
+public class MinCostMaketoEqualOne {
+
+	static int cost(int[] arr, int n) {
+		int min = arr[0];
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < min)
+				min = arr[i];
+		}
+		return (n - 1) * min;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []a = { 4, 3, 2 };
+        int n = a.length;
+        
+        System.out.println(cost(a, n));
+	}
+
+}
