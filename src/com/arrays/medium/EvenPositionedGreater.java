61733b29aeea97885fedb491bf7dc13585e24155
suniljakkula <sunij8911@gmail.com>
2026-02-09 06:10:33 +0530

    even positions greater

---
+++ src/com/arrays/medium/EvenPositionedGreater.java
@@ -0,0 +1,39 @@
+package com.arrays.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class EvenPositionedGreater {
+
+	static ArrayList<Integer> rearrangeArray(ArrayList<Integer> arr) {
+		Collections.sort(arr);
+
+		int n = arr.size();
+
+		ArrayList<Integer> result = new ArrayList<>(Collections.nCopies(n, 0));
+		int ptr1 = 0;
+		int ptr2 = n - 1;
+
+		for (int i = 0; i < n; i++) {
+			if ((i + 1) % 2 == 0) {
+				result.set(i, arr.get(ptr2--));
+			} else {
+				result.set(i, arr.get(ptr1++));
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 2, 1));
+
+		ArrayList<Integer> res = rearrangeArray(arr);
+
+		for (int num : res) {
+			System.out.print(num + " ");
+		}
+	}
+
+}
